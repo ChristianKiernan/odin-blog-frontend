55ad8a251ee12c2ba1cb89163d27c634ce561d5d
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-06-05 04:55:35 -0400

    Add initial authenticaton routes

---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import App from "./App";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import PostList from "./pages/PostList";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <PostList /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> }
+    ],
+  },
+];
+
+export default routes;
+
