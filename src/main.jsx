8d786fa2b5ce596636d9e65f13aa543d698dc42c
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-06-05 04:54:01 -0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import routes from "./routes";
+import "./index.css";
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
